f63424c6a5127bbfe819727ffb2e939ee522060d
DmitryS93 <d.siropyatov@mail.ru>
2023-05-03 11:32:23 +0300

    feat: add 01-read-file results

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,36 @@
 const fs = require('fs');
+const path = require('path');
+
+/*fs.readFile(
+  path.join(__dirname, 'text.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);*/
+
+/*const stream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));*/
+
+let stream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8');
+
+let data = '';
+stream.on('data', function(chunk) {
+  data = data + chunk;
+});
+
+stream.on('end', function(){
+  console.log(data);
+});
+
+stream.on('error', function(error){
+  console.log('Error', error.message);
+});
