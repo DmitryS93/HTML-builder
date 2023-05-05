0542d887d61a00cde8533aa7a9286f0271be63f9
DmitryS93 <d.siropyatov@mail.ru>
2023-05-05 11:56:30 +0300

    feat: task 06-build-page done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -56,29 +56,13 @@ const fs = require('fs');
 
 
 fs.readdir('./04-copy-directory', (err, files) => {
-    if(err) throw err; // не прочитать содержимое папки
-    //console.log(files.indexOf('files-copy'));
-    //console.log(files.indexOf('filess'));
-    if (files.indexOf('files-copy') >= 0) {
-        fs.rm(path.join(__dirname, 'files-copy'), {recursive:true}, err => {
-          if(err) throw err; // не удалось удалить папку
-          //console.log('Папка успешно удалена');
-          fs.mkdir(path.join(__dirname, 'files-copy'), err => {
-            if (err) throw err;
-            //console.log('Папка была создана');
-            fs.readdir('./04-copy-directory/files', (err, files) => {
-              if (err) console.log(err); 
-              for (let i=0; i<files.length; i++) {
-                //console.log(files[i]);
-                fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), err => {
-                  if(err) throw err;
-                  //console.log('done');
-                });
-              }
-            });
-          });  
-        });
-    } else {
+  if(err) throw err; // не прочитать содержимое папки
+  //console.log(files.indexOf('files-copy'));
+  //console.log(files.indexOf('filess'));
+  if (files.indexOf('files-copy') >= 0) {
+    fs.rm(path.join(__dirname, 'files-copy'), {recursive:true}, err => {
+      if(err) throw err; // не удалось удалить папку
+      //console.log('Папка успешно удалена');
       fs.mkdir(path.join(__dirname, 'files-copy'), err => {
         if (err) throw err;
         //console.log('Папка была создана');
@@ -92,8 +76,24 @@ fs.readdir('./04-copy-directory', (err, files) => {
             });
           }
         });
+      });  
+    });
+  } else {
+    fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+      if (err) throw err;
+      //console.log('Папка была создана');
+      fs.readdir('./04-copy-directory/files', (err, files) => {
+        if (err) console.log(err); 
+        for (let i=0; i<files.length; i++) {
+          //console.log(files[i]);
+          fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), err => {
+            if(err) throw err;
+            //console.log('done');
+          });
+        }
       });
-    }
-    console.log('done');
-  });
+    });
+  }
+  console.log('done');
+});
   
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,84 +1,35 @@
 const path = require('path');
 const fs = require('fs');
 
-//clear_folder();
-delete_folder();
-//create_folder();
-//styles();
 
-/*fs.readdir('./06-build-page', (err, files) => {
+fs.readdir('./06-build-page', (err, files) => {
   if(err) throw err; // не прочитать содержимое папки
   if (files.indexOf('project-dist') >= 0) {
-    fs.readdir('./06-build-page/project-dist', (err, files) => {
-      if (err) console.log(err); 
-      for (let i=0; i<files.length; i++) {
-        console.log(files[i]);
-        fs.unlink(path.join(__dirname, 'project-dist', files[i]), err => {
-          if(err) throw err; // не удалось удалить файл
-          console.log('Файл успешно удалён');
-        });
-      }
-      fs.rmdir(path.join(__dirname, 'project-dist'), err => {
-        if(err) throw err; // не удалось удалить папку
-        console.log('Папка успешно удалена');
-        fs.mkdir(path.join(__dirname, 'project-dist'), err => {
-          if (err) throw err;
-          console.log('Папка была создана');
-          styles();
-        });
-      });
+    fs.rm(path.join(__dirname, 'project-dist'), {recursive:true}, err => {
+      if(err) throw err; // не удалось удалить папку
+      //console.log('Папка успешно удалена');
+      create_folder();
+      build_html();
+      copy_assets();
     });
   } else {
-    fs.mkdir(path.join(__dirname, 'project-dist'), err => {
-      if (err) throw err;
-      console.log('Папка была создана');
-      styles();
-    });
+    create_folder();
+    build_html();
+    copy_assets();
   }
-});*/
+  console.log('done');
+});
 
 
-function delete_folder() {
-  fs.readdir('./06-build-page', (err, files) => {
-    if(err) throw err; // не прочитать содержимое папки
-    if (files.indexOf('project-dist') >= 0) {
-      fs.rm(path.join(__dirname, 'project-dist'), {recursive:true}, err => {
-        if(err) throw err; // не удалось удалить папку
-        console.log('Папка успешно удалена');
-      });
-    }
-  });
-}
-
-
-
-function clear_folder() {
-  fs.readdir('./06-build-page', (err, files) => {
-    if(err) throw err; // не прочитать содержимое папки
-    if (files.indexOf('project-dist') >= 0) {
-      fs.readdir('./06-build-page/project-dist', (err, files) => {
-        if (err) console.log(err); 
-        for (let i=0; i<files.length; i++) {
-          console.log(files[i]);
-          fs.unlink(path.join(__dirname, 'project-dist', files[i]), err => {
-            if(err) throw err; // не удалось удалить файл
-            console.log('Файл успешно удалён');
-          });
-        }
-      });
-    }
-  });
-}
-
-function create_folder() {
+async function create_folder() {
   fs.mkdir(path.join(__dirname, 'project-dist'), err => {
     if (err) throw err; //Не создать папку
-    console.log('Папка была создана');
+    //console.log('Папка была создана');
     styles();  
   });
 }
 
-function styles() {
+async function styles() {
   fs.readdir('./06-build-page/styles',{ withFileTypes: true }, (err, files) => {
     if(err) throw err;
     //console.log(files);
@@ -106,5 +57,80 @@ function styles() {
       }
     }
   });
-  console.log('Выполнен сбор стилей');
+  //console.log('Выполнен сбор стилей');
+}
+
+/*const readFile = (path) =>
+  new Promise((resolve, reject) =>
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    })
+  );*/
+
+const writeFile = (path, data) =>
+  new Promise((resolve, reject) =>
+    fs.writeFile(path, data, (err) => {
+      if (err) reject(err);
+      resolve();
+    })
+  );  
+
+async function build_html() {
+  fs.readFile(
+    path.join(__dirname, 'template.html'),
+    'utf-8',
+    (err, data) => {
+      if (err) throw err;
+      let result = data;
+      //console.log(tagsArray);
+      //console.log('прочитано');
+      fs.appendFile(
+        path.join(__dirname, 'project-dist', 'index.html'),
+        data,
+        err => {
+          if (err) throw err;
+          //console.log('Файл был изменен');
+          fs.readdir('./06-build-page/components', (err, files) => {
+            if(err) throw err; //проблема с файлами HTML в папке components
+            for (let i=0; i< files.length; i++) {
+              fs.readFile(
+                path.join(__dirname, 'components', files[i]),
+                'utf-8',
+                async (err, dataHTMLpart) => {
+                  if (err) throw err;
+                  //console.log(path.parse(files[i]).name);
+                  result = result.replace(`${path.parse(files[i]).name}`, dataHTMLpart);
+                  result = result.replace('{{', '');
+                  result = result.replace('}}', '');
+                  //console.log(result);
+                  await writeFile(path.join(__dirname, 'project-dist', 'index.html'), result);
+                });
+            }
+          });
+        }
+      );
+    }
+  );
+  //console.log('html собран');
+}
+
+async function copy_assets(){
+  fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), err => {
+    if (err) throw err;
+    //console.log('Папка была создана');
+    fs.cp(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'), {recursive: true}, (err) => {
+      if (err) console.log(err);   
+    });
+    /*fs.readdir('./06-build-page/assets', (err, files) => {
+      if (err) console.log(err); 
+      for (let i=0; i<files.length; i++) {
+      //console.log(files[i]);
+        fs.copyFile(path.join(__dirname, 'assets', files[i]), path.join(__dirname, 'project-dist', 'assets', files[i]), err => {
+          if(err) throw err; //Ошибка копирования assets
+        });
+      }
+    });*/
+  });
+  //console.log('done');
 }
